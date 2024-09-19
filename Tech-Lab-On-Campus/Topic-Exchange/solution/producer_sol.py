62440697c0cc0c465a9ee757b21f83ad7a7cc118
Jhon Tabio <JhonTabioCS@gmail.com>
2024-09-19 19:43:41 -0400

    Forgot one lil variable

--- Tech-Lab-On-Campus/Topic-Exchange/solution/producer_sol.py
+++ Tech-Lab-On-Campus/Topic-Exchange/solution/producer_sol.py
@@ -20,7 +20,7 @@ class mqProducer(mqProducerInterface):
         self.channel = self.connection.channel()
 
         if self.exchange == None:
-            self.exchange = self.exchange_declare(exchange=self.exchange_name, exchange_type="topic")
+            self.exchange = self.channel.exchange_declare(exchange=self.exchange_name, exchange_type="topic")
 
     def publishOrder(self, message: str) -> None:
         if self.connection == None or self.channel == None: return
